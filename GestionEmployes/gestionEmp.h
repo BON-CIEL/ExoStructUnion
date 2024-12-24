cbd30fb21493ac24476beb19fd77e653a45e3bc1
BON-CIEL <maximebonnevay.branly@gmail.com>
2024-12-24 11:57:20 +0100

    Commit #10

--- GestionEmployes/gestionEmp.h
+++ GestionEmployes/gestionEmp.h
@@ -14,3 +14,27 @@ struct Employe {
     string nom, prenom;
     Date dateEmbauche, datePrisePoste;
 };
+
+// Fonction de saisie de Date
+void saisirDate(Date& date) {
+    cout << "Jour : ";
+    cin >> date.jour;
+    cout << "Mois : ";
+    cin >> date.mois;
+    cout << "Annee : ";
+    cin >> date.annee;
+}
+
+// Fonction de saisie d'employé
+void saisirEmploye(Employe& employe) {
+    cout << "Nom : ";
+    cin >> employe.nom;
+    cout << "Prenom : ";
+    cin >> employe.prenom;
+
+    cout << "Date d'embauche :\n";
+    saisirDate(employe.dateEmbauche);
+
+    cout << "Date de prise de poste :\n";
+    saisirDate(employe.datePrisePoste);
+}
